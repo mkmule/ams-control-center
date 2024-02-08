27e45675dec6f79b31e5dd08823cb1bef3203aa1
Serhiy <serhiy.mos@gmail.com>
2024-02-08 16:47:41 +0400

    Replace file with ENV Variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -6,7 +6,11 @@ const env = process.env.NODE_ENV;
 if (!getApps().length) {
   if (env == 'development') {
     admin.initializeApp({
-      credential: admin.credential.cert(require('../../firebase-admin-account.json')),
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+        privateKey: (process.env.FIREBASE_ADMIN_PRIVATE_KEY || '').replace(/\\n/gm, '\n'),
+      }),
       databaseURL: 'https://ams24-ef028-default-rtdb.firebaseio.com',
     });
   } else {
